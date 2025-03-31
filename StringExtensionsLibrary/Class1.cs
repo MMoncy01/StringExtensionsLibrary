2424432985082f985dee422a3f73664727df340f
Mariya Ann Moncy <mmoncy1@my.centennialcollege.ca>
2025-03-30 21:08:07 -0400

    Initial commit

---
+++ StringExtensionsLibrary/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExtensionsLibrary
+{
+    public static class Class1
+    {
+        public static bool StartsWithUpperCase(this string input)
+        {
+            if (string.IsNullOrEmpty(input)) return false;
+            return char.IsUpper(input[0]);
+        }
+    }
+}
---
+++ TestStringExtensionApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using StringExtensionsLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string test1 = "Hello";
+        string test2 = "world";
+
+        Console.WriteLine($"\"{test1}\" starts with uppercase? {test1.StartsWithUpperCase()}");
+        Console.WriteLine($"\"{test2}\" starts with uppercase? {test2.StartsWithUpperCase()}");
+    }
+}
