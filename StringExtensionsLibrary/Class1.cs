5f7e6a86e8788cd17fc710bb5cea09b57d4fd5a5
Mariya Ann Moncy <mmoncy1@my.centennialcollege.ca>
2025-04-05 22:47:27 -0400

    Final: Working .NET 8 SDK-style project with NuGet pack support

---
+++ StringExtensionsLibrary/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace StringExtensionsLibrary
+{
+    public static class StringExtensions
+    {
+        public static bool StartsWithUpperCase(this string input)
+        {
+            if (string.IsNullOrEmpty(input)) return false;
+            return char.IsUpper(input[0]);
+        }
+    }
+}
